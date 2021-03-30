aced2fc5859f34cde785964e7a2ff4ab6c57399b
Zachary Maillard <zmaillard@protonmail.com>
2021-03-30 07:00:20 -0600

    Adding webpack

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path =require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                exclude: /node_modules/,
+                loader: 'file-loader',
+                options: {
+                    name:'[path][name].[ext]',
+                }
+            },
+            {
+                test: /\.elm$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                use: [
+                    {loader: 'elm-hot-webpack-loader'},
+                    {
+                        loader: 'elm-webpack-loader',
+                        options: {
+                            cwd: __dirname,
+                            debug: false
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    mode: 'development',
+
+    devServer: {
+        inline: true,
+        hot: true,
+        stats: 'errors-only',
+        contentBase: path.join(__dirname, "src"),
+    }
+}
