d18e61055cbe744a0d141f179d6a2f9b7320decd
Zachary Maillard <zmaillard@protonmail.com>
2021-04-02 06:52:52 -0600

    Removing Deploy Steps

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -1,15 +1,26 @@
+require('dotenv').config();
+
 const webpack = require("webpack");
 const path = require("path");
-const webpackMerge = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
     main: path.join(__dirname, "./src/index.js"),
   },
-
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: [
+          /node_modules/,
+        ],
+      },
+    ]
+  },
   plugins: [
+    new webpack.EnvironmentPlugin(["SEARCH_KEY", "SEARCH_SERVICE"]),
     new HtmlWebpackPlugin({
       template: "src/assets/index.html",
       inject: "body",
