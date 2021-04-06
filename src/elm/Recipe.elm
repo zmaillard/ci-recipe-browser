089fe2ad17af71c4ab4b880e12fc3c3f29269360
Zachary Maillard <zmaillard@protonmail.com>
2021-04-06 07:01:28 -0600

    Updating Record

--- src/elm/Recipe.elm
+++ src/elm/Recipe.elm
@@ -1,11 +1,13 @@
 module Recipe exposing (Recipe, recipesDecoder, recipeDecoder)
-import Json.Decode exposing (Decoder, int, list, string, map4, field)
+import Json.Decode exposing (Decoder, int, list, string, map6, field)
 
 type alias Recipe = 
   { issue : String
   , year : Int
   , recipe : String
   , category: String
+  , page: Int 
+  , month : List String
   }
 
 recipesDecoder : Decoder (List Recipe)
@@ -15,8 +17,10 @@ recipesDecoder =
 
 recipeDecoder : Decoder Recipe
 recipeDecoder =
-  map4 Recipe
+  map6 Recipe
     (field "issue" string)
     (field "year" int)
     (field "recipe" string)
     (field "category" string)
+    (field "page" int)
+    (field "months" (list string))
