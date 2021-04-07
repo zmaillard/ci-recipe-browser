c886d6fbd5639aca3e3f082451a663807550e3f6
Zach Maillard <zmaillard@protonmail.com>
2021-04-07 06:36:44 -0600

    Adding Page Numbers and Full Name (#5)
    * Updating Record
    
    * Adding Pages and Date Format

--- src/elm/Recipe.elm
+++ src/elm/Recipe.elm
@@ -1,12 +1,18 @@
-module Recipe exposing (Recipe, recipesDecoder, recipeDecoder)
-import Json.Decode exposing (Decoder, int, list, string, map4, field)
+module Recipe exposing (Recipe, recipesDecoder, recipeDecoder, formatDate)
+import Json.Decode exposing (Decoder, int, list, string, map6, field)
 
 type alias Recipe = 
   { issue : String
   , year : Int
   , recipe : String
   , category: String
+  , page: Int 
+  , month : List String
   }
+
+formatDate : Recipe -> String
+formatDate  recipe = 
+  String.join ", " recipe.month ++ " " ++ String.fromInt recipe.year
 
 recipesDecoder : Decoder (List Recipe)
 recipesDecoder =
@@ -15,8 +21,10 @@ recipesDecoder =
 
 recipeDecoder : Decoder Recipe
 recipeDecoder =
-  map4 Recipe
+  map6 Recipe
     (field "issue" string)
     (field "year" int)
     (field "recipe" string)
     (field "category" string)
+    (field "page" int)
+    (field "months" (list string))
